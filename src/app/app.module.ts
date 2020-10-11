0825747e6be8155abdc0b57c9c96bfb0f416ad2c
bentkowskiw <bentkowski@codogrosza.pl>
2020-10-11 20:44:21 +0200

    header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,22 @@ import { NgModule } from '@angular/core';
 /*import { AppRoutingModule } from './app-routing.module';*/
 import { AppComponent } from './app.component';
 import {PostCreateComponent} from './posts/post-create/post-create.component';
+import {HeaderComponent}  from './header/header.component';
+
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatInputModule } from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
    /* AppRoutingModule,*/
     PostCreateComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,6 +29,7 @@ import {MatButtonModule} from '@angular/material/button';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent,]
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 
-
-
-
-@Component ({
+@Component({
   selector: 'app-post-create',
-  templateUrl:'./post-create.component.html',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
 })
-export class PostCreateComponent  {
+export class PostCreateComponent {
   textAreaValue = '';
-  postValue='input value...';
-onAddPost() {
-  this.postValue = this.textAreaValue;
-}
+  postValue = 'input value...';
+  onAddPost() {
+    this.postValue = this.textAreaValue;
+  }
 }
